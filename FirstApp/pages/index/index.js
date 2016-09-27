f80e5e6f1c1b7025e1de1b25a693b8fa7a06a75f
jackytianhappy <1142899950@qq.com>
2016-09-27 16:11:16 +0800

    提交listview界面

--- FirstApp/pages/index/index.js
+++ FirstApp/pages/index/index.js
@@ -3,7 +3,29 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
+    componetsArr : [
+      {name:"View",id:"1"},
+      {name:"ScrollView","id":"2"},
+      {name:"Swiper","id":"3"},
+      {name:"Icon","id":"4"},
+      {name:"Text","id":"5"},
+      {name:"progress","id":"6"},
+      {name:"Button","id":"7"},
+      {name:"CheckBox","id":"8"},
+      {name:"Form","id":"9"},
+      {name:"Label","id":"10"},
+      {name:"Picker","id":"11"},
+      {name:"Radio","id":"12"},
+      {name:"Slider","id":"13"},
+      {name:"Switcher","id":"14"},
+      {name:"Action-sheet","id":"15"},
+      {name:"Modal","id":"16"},
+      {name:"Loading","id":"17"},
+      {name:"Audio","id":"18"},
+      {name:"Video","id":"19"},
+      {name:"Map","id":"20"},
+      {name:"Canvas","id":"21"}
+    ],
     userInfo: {}
   },
   //事件处理函数
